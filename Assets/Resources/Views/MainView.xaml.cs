899acb5b2fa63eb517d070ee7670e767c3009b9e
Stanislav Silin <byme.work@gmail.com>
2017-04-24 17:39:56 +0300

    Create base project with NoesisGUI

---
+++ Assets/Resources/Views/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace REDBlend.Assets.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
