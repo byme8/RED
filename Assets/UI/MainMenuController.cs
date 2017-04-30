ab619a0f7974f6274c2d14400c49be780835bda8
Stanislav Silin <byme.work@gmail.com>
2017-04-30 13:10:14 +0300

    Add plugingd for material design

---
+++ Assets/UI/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+    public void Play()
+    {
+        Debug.Log("Play");
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Settings");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+}
